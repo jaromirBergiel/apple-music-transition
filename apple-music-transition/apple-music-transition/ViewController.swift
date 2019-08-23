82015afd3503759051a8013742c17340ba5bdffb
Jaromir Bergiel <jaromir.bergiel@gmail.com>
2019-08-23 22:56:39 +0200

    [master] - Project initialized

---
+++ apple-music-transition/apple-music-transition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  apple-music-transition
+//
+//  Created by Jaromir Bergiel on 23/08/2019.
+//  Copyright © 2019 devberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
